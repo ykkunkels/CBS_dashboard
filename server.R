32b640c253c7c40f4a8d0472231fa27df3f521cd
ykkunkels <ykkunkels@gmail.com>
2023-07-10 14:35:50 +0200

    v0.0.2; initial data support

--- server.R
+++ server.R
@@ -1,85 +1,86 @@
 
 ################################
 ### TEST Shiny CBS Dashboard ###
-### Server version 0.0.1     ###
-### YKK - 06-06-2023         ###
+### Server version 0.0.2     ###
+### YKK - 12-06-2023         ###
 ###~*~*~*~*~*~*~*~*~*~*~*~*~*###
 
 # Define server ----
 server <- function(input, output, session) {
   
+  ## Define & initialise reactiveValues objects ----
+  SQL_input <- reactiveValues(connection_iSR = NA, jaar_transacties = NA, rekening_EB = NA, query = NA)
+  SQL_output <- reactiveValues(data = NA)
+  
   ## Define SQL connection ----
-  connection_iSR <- odbcDriverConnect("driver={SQL SERVER};server=SQL_HSR_ANA_PRD\\i01,50001;database=HSR_ANA_PRD;trusted_connection=true")
+  SQL_input$connection_iSR <- odbcDriverConnect("driver={SQL SERVER};server=SQL_HSR_ANA_PRD\\i01,50001;database=HSR_ANA_PRD;trusted_connection=true")
   
   ## Get CBS logo from external web-host ----
   output$logo <- renderUI({
     src <- "https://www.cbs.nl/Content/images/cbs-ld-logo.png"
-    div(id = "myImage",
-        tags$img(src = src, width = "65%", height = "auto"))
+    div(id = "myImage", tags$img(src = src, width = "65%", height = "auto"))
   })
   
   ## Settings: Role Selection ----
   output$role_txt <- renderText({
-    paste(input$selected_role, collapse = ", ")
+    paste("You are logged in as:", input$selected_role, collapse = ", ")
   })
   
+  # Only show load data button when role check box is ticked
   observe({
-    toggle(id = "role_button", condition = input$selected_role)
+    toggle(id = "load_data_button", condition = input$selected_role)
   })
   
-  
-  ## Data Preparation: DT ----
-  # Prepare the large datasets outside the reactive environment
-  data1 <- reactive({
-    if (input$selected_role == "Integrator") {
-      # Load and return Integrator data
-      return(iris)
-    }
-  })
-  
-  data2 <- reactive({
-    if (input$selected_role == "DNB expert") {
-      # Load and return DNB expert data
-      return(mtcars)
-    }
-  })
-  
-  data3 <- reactive({
-    if (input$selected_role == "R expert") {
-      # Load and return R expert data
-      return(ChickWeight)
-    }
-  })
-  
-  # Combine the selected datasets into a single reactive expression
-  combinedData <- reactive({
-    data <- list(data1(), data2(), data3())
-    data <- Filter(function(x) !is.data.frame(x) || nrow(x) > 0, data)  # Remove empty data frames from the list
-    data
-  })
-  
-  # Render the selected data using the DT::renderDataTable function
-  output$selectedData <- renderDT({
-    data <- combinedData()
-    
-    if(length(data) > 0) {
-      # Combine and display the selected datasets in a single DataTable
-      datatable(
-        do.call(rbind, data),
-        options = list(scrollX = TRUE)  # Enable horizontal scrolling if needed
-      )
-    }
-  })
-  
-  
-  ## Settings: Load data button ----
-  observeEvent(input$role_button, { # Jump to Data tab when role is selected
+  # Jump to Data tab when load data button is clicked
+  observeEvent(input$load_data_button, {
     updateTabItems(session, "sidebarmenu", "data_tab")
   })
   
   ## Observe: Excel button click---- 
   observeEvent(input$excel_button, {
     shell.exec("F:/Desktop/Dashboard_EXAMPLE/Example_Dashboard_6.6_2022V_230531.xlsb")
+  })
+  
+  ## Data Module: Preparation & Loading ----
+  # Load and return the large data sets outside the reactive environment, for each role
+  data1 <- reactive({
+    if (input$selected_role == "Integrator") {
+      return(iris)
+    }
+  })
+  
+  data2 <- reactive({
+    if (input$selected_role == "DNB expert") {
+      return(mtcars)
+    }
+  })
+  
+  data3 <- reactive({
+    if (input$selected_role == "R expert") {
+      SQL_input$jaar_transacties <- "2021"
+      SQL_input$rekening_EB <- "EB"
+      SQL_input$query <- paste0("SELECT Jaar, Periode, Status
+                                 FROM 	tbl_SR_Data_Transacties  
+                                 WHERE 	Jaar >= ('",SQL_input$jaar_transacties,"') AND Rekening = ('",SQL_input$rekening_EB,"')
+                                 GROUP BY Jaar, Periode, Status")
+      
+      SQL_output$data <- sqlQuery(SQL_input$connection_iSR, SQL_input$query)
+      
+      # return(SQL_output$data)
+    }
+  })
+  
+  # Combine into a single reactive expression
+  combinedData <- reactive({
+    output_data <- list(data1(), data2(), data3())
+  })
+  
+  # Combine and render the selected data in a single datatable
+  output$selectedData <- renderDT({
+    if (!is.null(input$selected_role)) {
+      output_data <- combinedData()
+      datatable(do.call(rbind, output_data), options = list(scrollX = TRUE)) # horizontal scrolling is TRUE
+    } 
   })
   
 } # closing server{}
--- ui.R
+++ ui.R
@@ -1,8 +1,8 @@
 
 ################################
 ### TEST Shiny CBS Dashboard ###
-### UI version 0.0.1         ###
-### YKK - 06-06-2023         ###
+### UI version 0.0.2         ###
+### YKK - 12-06-2023         ###
 ###~*~*~*~*~*~*~*~*~*~*~*~*~*###
 
 ## Load and / or Install required packages ----
@@ -11,6 +11,7 @@ if(!require('shinydashboard')){install.packages('shinydashboard', dep = TRUE)};l
 if(!require('shinyjs')){install.packages('shinyjs', dep = TRUE)};library('shinyjs')
 if(!require('RODBC')){install.packages('RODBC', dep = TRUE)};library('RODBC') # Misschien ODBC 
 if(!require('DT')){install.packages('DT', dep = TRUE)};library('DT')
+if(!require('shinycssloaders')){install.packages('shinycssloaders', dep = TRUE)};library('shinycssloaders')
 
 ## UI ----
 # Define UI for application that draws a histogram
@@ -20,7 +21,7 @@ ui <- dashboardPage(skin = "blue",
                     dashboardHeader(title = "CBS Dashboard"),
                     
                     ## Sidebar ----
-                    dashboardSidebar(width = 250,
+                    dashboardSidebar(width = 230,
                                      sidebarMenu(menuItem("Menu"), id = "sidebarmenu",
                                                  menuItem("Settings", tabName = "settings_tab", icon = icon("cog")),
                                                  menuItem("Data", tabName = "data_tab", icon = icon("file-alt")),
@@ -28,9 +29,8 @@ ui <- dashboardPage(skin = "blue",
                                                  menuItem("Intranet", icon = icon("atlas"), href = "https://cbsintranet/default.aspx/"),
                                                  menuItem("Checks", tabName = "checks_tab", icon = icon("book-reader")),
                                                  uiOutput("logo", style = "background-color: white;"),
-                                                 h5("version 0.0.1", style = "font-style: normal; letter-spacing: 1px; line-height: 26pt;
-                position: absolute; bottom: 0; left: 40px;")
-                                                 
+                                                 h5("version 0.0.2", style = "font-style: normal; letter-spacing: 1px; line-height: 26pt;
+                                                    position: relative; left: 30px;")
                                      ) # closing sidebarMenu()
                     ), # closing dashboardSidebar()
                     
@@ -45,25 +45,23 @@ ui <- dashboardPage(skin = "blue",
                         tabItem(tabName = "settings_tab", # Settings tab ----
                                 
                                 ## Role selection ----
-                                checkboxGroupInput("selected_role", "Select your role to customise data selection:", inline = TRUE,
+                                checkboxGroupInput("selected_role", "Select your role to customise data selection:", 
+                                                   inline = TRUE, selected = "Integrator",
                                                    c("Integrator" = "Integrator",
                                                      "DNB expert" = "DNB expert",
                                                      "R expert" = "R expert")
                                 ),
-                                h5("Your current role is:"),
-                                textOutput(outputId = "role_txt"),
                                 
                                 ## Input: Role button
-                                actionButton(inputId= "role_button", label = "Load your data"),
+                                actionButton(inputId= "load_data_button", label = paste("Load your data")),
                                 
                         ), # closing Settings tabItem()
                         
                         tabItem(tabName = "data_tab", # Data tab ----
                                 
-                                h5("Here be dragons"),
-                                
-                                # verbatimTextOutput("selectedData")
-                                DTOutput("selectedData")
+                                ## Loaded Data: DT ----
+                                textOutput(outputId = "role_txt"), br(),
+                                withSpinner(DTOutput("selectedData"), type = 6)
                                 
                         ), # closing Data tabItem()
                         
@@ -75,10 +73,8 @@ ui <- dashboardPage(skin = "blue",
                         
                         tabItem(tabName = "checks_tab", # Checks tab ----
                                 
-                                ## Text Explain: Excel button
-                                h5("To open the dashboard in Excel, click the button below."),
-                                
                                 ## Input: Excel button
+                                h5("To open the dashboard in Excel, click the button below."),
                                 actionButton(inputId= "excel_button", label = "Open Excel Dashboard")
                                 
                         ) # closing Checks tabItem()
